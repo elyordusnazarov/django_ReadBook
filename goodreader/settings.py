b2c84007f1ece053fa5c045914fd398d7396df43
Elyor <dusnazarov.ed@gmail.com>
2026-02-15 17:07:02 +0500

    'commited'

--- goodreader/settings.py
+++ goodreader/settings.py
@@ -80,17 +80,24 @@ WSGI_APPLICATION = 'goodreader.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE':'django.db.backends.postgresql_psycopg2',
+#         'NAME':'goodreads',
+#         'HOST':'localhost',
+#         'PORT':'5432',
+#         'USER':'postgres',
+#         'PASSWORD':'ed1115ed'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME':'goodreads',
-        'HOST':'localhost',
-        'PORT':'5432',
-        'USER':'postgres',
-        'PASSWORD':'ed1115ed'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+                 
 
 
 # Password validation
